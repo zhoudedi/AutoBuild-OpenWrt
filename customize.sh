62aad85257298d1b833e3f54a1bfe9f0c1c846ae
zhoudedi <64512474+zhoudedi@users.noreply.github.com>
2022-02-12 02:42:53 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,6 +7,7 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+git clone https://github.com/OpenWrt-Actions/openwrt-package-lienol package/openwrt-package-lienol
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
